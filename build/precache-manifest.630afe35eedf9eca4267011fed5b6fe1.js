35de8fae4ec19c675c50d6373a47d61739bdcb63
cqxg <cqxg@yandex.ru>
2019-08-18 12:01:14 +0300

    feat: add build folder

---
+++ build/precache-manifest.630afe35eedf9eca4267011fed5b6fe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf4cc0bea1642c8ecc0d7ba9bfc2817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2eff8bbee947e1966fe",
+    "url": "/static/css/main.d6df939f.chunk.css"
+  },
+  {
+    "revision": "120060f34a9b742956ae",
+    "url": "/static/js/2.8157049c.chunk.js"
+  },
+  {
+    "revision": "d2eff8bbee947e1966fe",
+    "url": "/static/js/main.a15130b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
