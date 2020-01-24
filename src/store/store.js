e419f0f2a1e096c523ae6cdbefc84a8aaf9d3d1f
cqxg <cqxg@yandex.ru>
2020-01-24 11:48:30 +0300

    feat: add store workpiece

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from 'redux';
+
+import { loginReducer } from './reducers/LoginReducer';
+import { mainReducer } from './reducers/MainReducer';
+
+const reducers = combineReducers({
+    loginPage: loginReducer,
+    mainPage: mainReducer,
+});
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default store;
