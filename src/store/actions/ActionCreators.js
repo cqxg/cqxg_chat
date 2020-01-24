d534f882afee0f335aa53fd96027306939fd7ebe
cqxg <cqxg@yandex.ru>
2020-01-24 11:40:34 +0300

    feat: add action creators workpiece

--- src/store/actions/ActionCreators.js
+++ src/store/actions/ActionCreators.js
@@ -1,1 +1,7 @@
 import { LOGIN, RELOGIN, SEND_MESSAGE } from "./ActionTypes";
+
+const login = () => ({ type: LOGIN });
+const relogin = () => ({ type: RELOGIN });
+const sendMessage = () => ({ type: SEND_MESSAGE });
+
+export { login, relogin, sendMessage }
