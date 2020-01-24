0824853dcc34ac0b93db9da818d976b86ffed851
cqxg <cqxg@yandex.ru>
2020-01-24 11:17:34 +0300

    feat: add store structure preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 import LoginForm from './components/LoginForm';
-import connecting from './store/Store';
 
 const App = () => {
-    connecting();
 
     return (
         <div>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
-const LoginForm = () => {
-    return (
-        <span>Login here</span>
-    )
+class LoginForm extends Component {
+
+    componentDidMount() {
+        const websocket = new WebSocket('wss://wssproxy.herokuapp.com/ ');
+        websocket.onopen = () => {
+            console.log('open');
+        }
+        websocket.onmessage = (e) => {
+            const messages = JSON.parse(e.data);
+            console.log(messages);
+        };
+    };
+
+    render() {
+        return (
+            <div>
+                Login Form
+            </div>
+        )
+    }
 };
 
 export default LoginForm;
