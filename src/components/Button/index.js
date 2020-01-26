da442e37d0101eab7d665f0d3aa42f5e76a18b5f
cqxg <cqxg@yandex.ru>
2020-01-26 21:55:33 +0300

    feat: add button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames'
+import { Button as BaseButton } from 'antd';
+
+const Button = (props) => {
+    return (
+        <BaseButton
+            {...props}
+            className={classNames('button', props.className, {
+                'button--large': props.size === 'large'
+            })}
+        />
+    );
+
+}
+
+Button.propTypes = {
+    className: PropTypes.string,
+}
+
+export default Button;
