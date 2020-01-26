3e88b075bea8bcab5664b405b7bbe8e3390f7c27
cqxg <cqxg@yandex.ru>
2020-01-26 21:53:19 +0300

    feat: add block styles

--- src/components/Block/Block.js
+++ src/components/Block/Block.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames'
 
+import './Block.scss'
+
 const Block = ({ children, className }) => (
     <div className={classNames('block', className)}>{children}</div>
 );
