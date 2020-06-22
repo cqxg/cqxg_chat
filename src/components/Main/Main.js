cf1148da01cf64fe131c5eb577f3adba2b088f92
unknown <cqxg@yandex.ru>
2020-06-22 22:25:13 +0300

    feat: add new dependencies

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { AVATAR, RELOGIN } from '../../utils/const';
@@ -8,101 +8,187 @@ import Button from '../Button/Button';
 
 import './Main.scss';
 
-class Main extends Component {
+// const Main = () => {
+//     const [state, setState] = useState({
+//         user: 'user',
+//         messages: [{
+//             from: 'Vasya'
+//         }],
+//         connect: false,
+//     });
 
-    constructor() {
-        super();
-        this.state = {
-            user: 'user',
-            messages: [],
-            connect: false,
-        };
+//     let _websocket = null;
+//     const chatEnd = React.createRef();
 
-        this._websocket = null;
-        this.chatEnd = React.createRef();
-    };
+//     const connect = () => {
+//         _websocket = new WebSocket('ws://localhost:3030/');
+//         _websocket.onopen = () => {
+//             setState({ connect: true });
+//         };
 
-    componentDidMount() {
-        this.connect();
-    };
+//         _websocket.onmessage = (e) => {
+//             const messages = JSON.parse(e.data).reverse();
+//             setState({ messages: [...state.messages, ...messages] });
+//             chatEnd.current.scrollIntoView({ block: 'nearest' })
+//         };
 
-    connect = () => {
-        this._websocket = new WebSocket('wss://wssproxy.herokuapp.com/');
-        this._websocket.onopen = () => {
-            this.setState({ connect: true });
-        };
+//         _websocket.onclose = () => {
+//             setState({ connect: false });
+//             setTimeout(() => {
+//                 connect();
+//             }, 1000);
+//         };
+//     };
 
-        this._websocket.onmessage = (e) => {
-            const messages = JSON.parse(e.data).reverse();
-            this.setState(
-                (state) => ({ messages: [...state.messages, ...messages] }),
-                () => this.chatEnd.current.scrollIntoView({ block: 'nearest' })
-            );
-        };
+//     useEffect(() => connect(), []);
 
-        this._websocket.onclose = () => {
-            this.setState({ connect: false });
-            setTimeout(() => {
-                this.connect();
-            }, 1000);
-        };
-    };
+//     const mappedMessages = () => {
+//         const newMap = state.messages.map((user) => (
+//             <div className={user.from === localStorage.name ? 'message--isme' : 'message'} key={user.time} >
+//                 <div className='message__avatar' >
+//                     <img src={AVATAR} />
+//                 </div>
+//                 <div className='message__content'>
+//                     <div className='message__bubble'>
+//                         <p className='message__text'>{user.message}</p>
+//                     </div>
+//                     <span className='message__sender' ref={chatEnd} >{user.from}</span>
+//                 </div>
+//             </div>
+//         ));
+//     };
 
-    goMap = () => {
-        const newMap = this.state.messages.map((user) => (
-            <div className={user.from === localStorage.name ? 'message--isme' : 'message'} key={user.time} >
-                <div className='message__avatar' >
-                    <img src={AVATAR} />
-                </div>
-                <div className='message__content'>
-                    <div className='message__bubble'>
-                        <p className='message__text'>{user.message}</p>
-                    </div>
-                    <span className='message__sender' ref={this.chatEnd} >{user.from}</span>
-                </div>
-            </div>
-        ));
+//     const changeName = (newName = 'user') => {
+//         setState({ user: newName });
+//         localStorage.setItem('name', JSON.stringify(newName));
+//     };
 
-        return newMap;
-    };
+//     const sendMessage = (e) => {
+//         if (localStorage.name === undefined) {
+//             localStorage.name = 'Anonymous';
+//         };
 
-    changeName = (newName = 'user') => {
-        this.setState({ user: newName });
-        localStorage.setItem('name', JSON.stringify(newName));
-    };
+//         let text = e.target.value;
+//         const message = {
+//             message: `${text}`,
+//             from: `${localStorage.name}`,
+//         };
 
-    sendMessage = (e) => {
-        if (localStorage.name === undefined) {
-            localStorage.name = 'Anonymous';
-        };
+//         if (e.key === 'Enter') {
+//             e.target.value = '';
+//             _websocket.send(JSON.stringify(message));
+//         };
+//     };
 
-        let text = e.target.value;
-        const message = {
-            message: `${text}`,
-            from: `${localStorage.name}`,
-        };
+//     return (
+//         <div className='wrapper'>
+//             <NavLink to='/'>
+//                 <Button type='primary' size='small'>
+//                     {RELOGIN}
+//                 </Button>
+//             </NavLink>
+//             <div className={state.connect === false ? 'nocontent' : 'content'}>
+//                 {state.connect === false ? <Preloader /> : mappedMessages()}
+//             </div>
+//             <input onKeyPress={sendMessage} className='myText' type='text' placeholder='Enter your message...' />
+//         </div>
+//     );
+// };
 
-        if (e.key === 'Enter') {
-            e.target.value = '';
-            this._websocket.send(JSON.stringify(message));
-        };
-    };
+// export default Main;
 
-    render() {
-        return (
-            <div className='wrapper'>
-                <NavLink to='/'>
-                    <Button type='primary' size='small'>
-                        {RELOGIN}
-                    </Button>
-                </NavLink>
-                <div className={this.state.connect === false ? 'nocontent' : 'content'}>
-                    {this.state.connect === false ? <Preloader /> : this.goMap()}
-                </div>
-                <input onKeyPress={this.sendMessage} className='myText' type='text' placeholder='Enter your message...' />
-            </div>
-        );
-    };
-};
 
-export default Main;
+// constructor() {
+    //         super();
+    //         this.state = {
+    //             user: 'user',
+    //             messages: [],
+    //             connect: false,
+    //         };
+
+    //         this._websocket = null;
+    //         this.chatEnd = React.createRef();
+    //     };
+
+    //     componentDidMount() {
+    //         this.connect();
+    //     };
+
+    //     connect = () => {
+    //         this._websocket = new WebSocket('wss://wssproxy.herokuapp.com/');
+    //         this._websocket.onopen = () => {
+    //             this.setState({ connect: true });
+    //         };
+
+    //         this._websocket.onmessage = (e) => {
+    //             const messages = JSON.parse(e.data).reverse();
+    //             this.setState(
+    //                 (state) => ({ messages: [...state.messages, ...messages] }),
+    //                 () => this.chatEnd.current.scrollIntoView({ block: 'nearest' })
+    //             );
+    //         };
+
+    //         this._websocket.onclose = () => {
+    //             this.setState({ connect: false });
+    //             setTimeout(() => {
+    //                 this.connect();
+    //             }, 1000);
+    //         };
+    //     };
+
+    //     goMap = () => {
+    //         const newMap = this.state.messages.map((user) => (
+    //             <div className={user.from === localStorage.name ? 'message--isme' : 'message'} key={user.time} >
+    //                 <div className='message__avatar' >
+    //                     <img src={AVATAR} />
+    //                 </div>
+    //                 <div className='message__content'>
+    //                     <div className='message__bubble'>
+    //                         <p className='message__text'>{user.message}</p>
+    //                     </div>
+    //                     <span className='message__sender' ref={this.chatEnd} >{user.from}</span>
+    //                 </div>
+    //             </div>
+    //         ));
+
+    //         return newMap;
+    //     };
+
+    //     changeName = (newName = 'user') => {
+    //         this.setState({ user: newName });
+    //         localStorage.setItem('name', JSON.stringify(newName));
+    //     };
+
+    //     sendMessage = (e) => {
+    //         if (localStorage.name === undefined) {
+    //             localStorage.name = 'Anonymous';
+    //         };
+
+    //         let text = e.target.value;
+    //         const message = {
+    //             message: `${text}`,
+    //             from: `${localStorage.name}`,
+    //         };
+
+    //         if (e.key === 'Enter') {
+    //             e.target.value = '';
+    //             this._websocket.send(JSON.stringify(message));
+    //         };
+    //     };
+
+    //     render() {
+    //         return (
+    //             <div className='wrapper'>
+    //                 <NavLink to='/'>
+    //                     <Button type='primary' size='small'>
+    //                         {RELOGIN}
+    //                     </Button>
+    //                 </NavLink>
+    //                 <div className={this.state.connect === false ? 'nocontent' : 'content'}>
+    //                     {this.state.connect === false ? <Preloader /> : this.goMap()}
+    //                 </div>
+    //                 <input onKeyPress={this.sendMessage} className='myText' type='text' placeholder='Enter your message...' />
+    //             </div>
+    //         );
+    //     };
+    // };
