6f8d9bf2cee4bdf9b593cf2def2d772680b5131f
unknown <cqxg@yandex.ru>
2020-07-09 19:55:12 +0300

    feat: add new preloader

--- src/components/AsideBar/AsideBar.js
+++ src/components/AsideBar/AsideBar.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
 import online from '../../utils/online.png';
+import Preloader from '../../utils/preloader';
 
 import './Aside.scss';
 
 const AsideBar = ({ users }) => (
   <div className="asideBar">
     {users
-        ? (
-          <div>
-            <h2>People currently chatting:</h2>
-            <div className="activeContainer">
-              <h3>
-                {users.map(({ name }) => (
-                  <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={online} />
-                  </div>
-                ))}
-              </h3>
-            </div>
+      ? (
+        <div>
+          <h2>People currently chatting:</h2>
+          <div className="activeContainer">
+            <h3>
+              {users.map(({ name }) => (
+                <div key={name} className="activeItem">
+                  {name}
+                  <img alt="Online Icon" src={online} />
+                </div>
+              ))}
+            </h3>
           </div>
-        )
-        : null}
+        </div>
+      )
+      : <Preloader />}
   </div>
 );
 
